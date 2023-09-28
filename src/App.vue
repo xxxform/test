767b8b724ef7d39abf3eb41797123a5b3ee3957d
Sergey Bit <apo-bit@yandex.ru>
2023-09-28 15:49:15 +0300

    asyncContentLoading

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="app">
+        <h1>Страница с постами</h1>
+        <my-button style="margin: 15px 0;" @click="showDialog">Создать пост</my-button>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form @create="createPost"/>
+        </my-dialog>
+        <post-list 
+            :posts="posts"
+            @remove="removePost"
+            v-if="!isPostsLoading"
+        />
+        <div v-else>Идёт загрузка...</div>
+    </div>
 </template>
 
+<script>
+import PostForm from '@/components/PostForm';
+import PostList from '@/components/PostList';
+
+export default {
+    components: {
+        PostForm, PostList
+    },
+    data() {
+        return {
+            posts: [],
+            dialogVisible: false,
+            isPostsLoading: false,
+
+        }
+    },
+    mounted() {
+        this.fetchPosts();
+    }, 
+    methods: { 
+        async fetchPosts() {
+            this.isPostsLoading = true;
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                const json = await response.json();
+                this.posts.push(...json);
+            } catch (e) {
+                alert('Произошла ошибка при загрузке');
+            } finally {
+                this.isPostsLoading = false;
+            }
+        },
+        showDialog() {
+            this.dialogVisible = true;
+        },
+        removePost(post) {
+            this.posts = this.posts.filter(p => p.id !== post.id);
+        }, 
+        createPost(newPost) {
+            this.posts.push(newPost);
+            this.dialogVisible = false;
+        }
+    },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+    padding: 20px;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-if="posts.length > 0">
+        <h3>Список постов</h3>
+        <post-item 
+            v-for="post in posts" 
+            :key="post.id"
+            :post="post" 
+            @remove="$emit('remove', post)"
+        />
+    </div>
+    <h2 v-else style="color: red">Список постов пуст</h2>
+</template>
+
+<script>
+import PostItem from '@/components/PostItem';
+
+export default {
+    components: {PostItem},
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
